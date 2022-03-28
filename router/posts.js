bde50af652e5d438bcf19c02f80551edf7aad153
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-03-28 21:37:27 +0900

    일단 커밋

--- router/posts.js
+++ router/posts.js
@@ -37,7 +37,7 @@ router.post('/auth', async (req, res) => {
     // DB에 입력된 비밀번호 복호화 하기
     const userpassword = await User.findOne({ id }).then((value) => { return value.password })
     // 복호화된 비밀번호와 입력된 비밀번호 일치하는지 확인하기
-    const passwordcheck = await bcrypt.compare(password, userpassword).then((value) => { return value })
+    const passwordcheck = await bcrypt.compare(password, userpassword).then((value) => { return value }).catch((error) => { res.status(400).send({ errmsg: '다시 시도해주세요.' }) })
     console.log(password, passwordcheck)
     // 일치하지 않으면, status 400, errormsg 보내기
     if (!passwordcheck) {
@@ -102,8 +102,13 @@ router.post('/upload', async (req, res) => {
     const { tokenid, nickname, title, password, content } = req.body
     // 토큰을 가진 사용자의 DB ID 가져오기
     const { userId } = jwt.verify(tokenid, 'my-secret-key')
-    const userId_DB = await User.findOne({ userId }).then((value) => { return value._id.toHexString() })
+    console.log(userId)
+    console.log(userId)
+    const userId_DB = await User.findOne({ _id: userId }).then((value) => { return value._id.toHexString() })
+    console.log(userId_DB)
     const date = new Date()
+    // 비밀번호 암호화하기.
+    const pw_hash = await bcrypt.hash(password, saltRounds).then((value) => { return value })
     let count = 0
     posts = await Posts.find({})
     if (posts.length === 0) {
@@ -117,7 +122,7 @@ router.post('/upload', async (req, res) => {
         userId_DB,
         nickname,
         title,
-        password,
+        password: pw_hash,
         content,
         date,
         count // 각 게시글에 고유값 지정 위해서...
@@ -133,7 +138,6 @@ router.get('/:count', async (req, res) => {
     // res.json({ success: true, post: post })
     res.render('../views/detail', { post })
 })
-
 // 수정 페이지 이동
 router.get('/:count/edit', async (req, res) => {
     const Count = req.params.count
@@ -141,33 +145,40 @@ router.get('/:count/edit', async (req, res) => {
     res.render('../views/edit', { post })
 })
 
+// 게시물 수정 API: 입력값과 비밀번호 bcryt 푼 값이 동일한지 확인 추가
 router.put('/:count/edit', async (req, res) => {
     const Count = req.params.count
-    // 비밀번호, 수정 내용 가져오기
-    const { pw, comment } = req.body
+    // request된 비밀번호, 수정 내용 가져오기
+    const { content, password } = req.body
     const existpost = await Posts.find({ count: Number(Count) }, { _id: false })
+    // existpost의 암호화된 비밀번호와 request받은 비밀번호 비교하기.
+    const postpassword = existpost[0].password
+    const passwordcheck = await bcrypt.compare(password, postpassword).then((value) => { return value }).catch((error) => { res.status(400).send({ errmsg: '다시 시도해주세요.' }) })
     // 게시글이 존재하고, 비밀번호가 동일하면 삭제
     if (existpost.length) {
-        if (Number(pw) === Number(existpost[0].pw)) {
-            await Posts.updateOne({ count: Number(Count) }, { $set: { comment } })
+        if (passwordcheck) {
+            await Posts.updateOne({ count: Number(Count) }, { $set: { content } })
             res.json({ success: true, msg: '수정 완료했습니다.' })
         }
-        else {
-            res.json({ success: false, errormsg: '비밀번호가 틀립니다.' })
-        }
+    }
+    else {
+        res.json({ success: false, errormsg: '비밀번호가 틀립니다.' })
     }
 })
 
 //  상세 게시글 삭제
 router.delete('/:count/edit', async (req, res) => {
     const Count = req.params.count
-    // 비밀번호, 수정 내용 가져오기
-    const { pw } = req.body
+    // 비밀번호 가져오기
+    const { password } = req.body
+    // 포스트 DB에서 상세 게시글 데이터 가져오기
     const existpost = await Posts.find({ count: Number(Count) }, { _id: false })
-
+    // 포스트 DB에서 비밀번호 복호화해서 입력값 비밀번호와 비교하기
+    const postpassword = existpost[0].password
+    const passwordcheck = await bcrypt.compare(password, postpassword).then((value) => { return value }).catch((error) => { res.status(400).send({ errmsg: '다시 시도해주세요.' }) })
     // 게시글이 존재하고, 비밀번호가 동일하면 삭제
     if (existpost.length) {
-        if (Number(pw) === Number(existpost[0].pw)) {
+        if (passwordcheck) {
             await Posts.deleteOne({ count: Number(Count) })
             res.json({ success: true, msg: '삭제했습니다.' })
         }
@@ -176,8 +187,11 @@ router.delete('/:count/edit', async (req, res) => {
         }
     }
 })
+// 상세 게시글 - 댓글 POST API
 
-
-
-
+//router.post()
+// 댓글 POST API with DB 스키마 제작(nickname, comment, userID_DB, contentID_DB)
+// 댓글 GET API to 상세페이지
+// 댓글 patch API
+// 댓글 delete API
 module.exports = router
